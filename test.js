2885bbbc6f7e294ef5577f10270e282afce1ad04
Paresh <pareshnathnandi1234@gmail.com>
2024-02-28 13:18:14 +0530

    Recomended files for portfolio

---
+++ test.js
@@ -0,0 +1,8 @@
+var bonuspoints = 50 ;
+console.log(bonuspoints);
+bonuspoints = bonuspoints + 50;
+console.log(bonuspoints);
+bonuspoints = bonuspoints - 75;
+console.log(bonuspoints);
+bonuspoints  = bonuspoints + 45;
+console.log(bonuspoints);
